7da786eb9325077c823fc9351044e822312e51ee
Nari Park <nari36@naver.com>
2023-03-29 22:21:38 -0400

    fixed error

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,41 +1,45 @@
-const User = require('../models/user');
+const User = require('../models/userModel');
 
 const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  // Validation
-  if (!name ||!email ||!password) {
-    return res.status(400).json({ error: 'Please fill all fields' });
-  };
-  if (password.length < 8) {
-    return res.status(400).json({ error: 'Password must be at least 8 characters' });
-  };
+    // Validation
+    if (!name ||!email ||!password) {
+      return res.status(400).json({ error: 'Please fill all fields' });
+    };
+    if (password.length < 8) {
+      return res.status(400).json({ error: 'Password must be at least 8 characters' });
+    };
 
-  // Check if user already exists
-  const user = await User.findOne({ email });
-  if (user) {
-    return res.status(400).json({ error: 'User already exists' });
-  };
+    // Check if user already exists
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: 'User already exists' });
+    };
 
-  // Create new user
-  const newUser = new User({
-    name,
-    email,
-    password
-  });
+    // Create new user
+    const newUser = new User({
+      name,
+      email,
+      password
+    });
 
-  if (user) {
-    const { _id, name, email, password, photo, phone, bio } = user;
-    return res.status(201).json({
-      _id, name, email, password, photo, phone, bio
-    })
-  } else {
-    return res.status(400).json({ error: 'Invalid user data' });
+    if (user) {
+      const { _id, name, email, password, photo, phone, bio } = user;
+      return res.status(201).json({
+        _id, name, email, password, photo, phone, bio
+      })
+    } else {
+      return res.status(400).json({ error: 'Invalid user data' });
+    }
   }
-  
+  catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 
 }
 
 
 
-module.exports = registerUser;
+module.exports = { registerUser };
