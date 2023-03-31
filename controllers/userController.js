064b72ef6006f3a9f8c3f8548dd94f636f20ce5a
Nari Park <nari36@naver.com>
2023-03-31 13:45:10 -0400

    created loginUser controller&route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,14 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
+// Generate Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 };
 
+// Register User
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -59,6 +62,50 @@ const registerUser = async (req, res) => {
 
 }
 
+// Login User
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validation
+    if (!email ||!password) {
+      return res.status(400).json({ error: 'Please fill all fields' });
+    } 
+
+    // Check if user exits
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'User does not exist, please sign up' });
+    }
+
+    // User exists, check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // Generate Token
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+      path: '/',
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400),
+      sameSite: "none",
+      secure: true
+    });
+
+    if (user && isMatch) {
+      const { _id, name, email, photo, phone, bio } = user;
+      return res.status(200).json({
+        _id, name, email, photo, phone, bio, token
+      })
+    } else {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
 
 
-module.exports = { registerUser };
+module.exports = { registerUser, loginUser };
