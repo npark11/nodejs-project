cc568afe50161d8a73a9185d8c169cda9b51cc43
Nari Park <nari36@naver.com>
2023-05-22 16:37:11 -0400

    added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ["http://localhost:3000", "https://lemontree-inventory-app.onrender.com"],
+  origin: ["http://localhost:3000", "https://lemontree-inventory-app.onrender.com", "https://lemontree-blog.onrender.com"],
   credentials: true,
 }));
 
